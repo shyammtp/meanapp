a9c72cba30652280e3d1fde7a0f7b7af251c6db9
shyammtp <shyam.mtp@gmail.com>
2017-01-24 21:06:31 +0530

    Shyam Commit

--- packages/custom/backend/public/directives/listwidget.js
+++ packages/custom/backend/public/directives/listwidget.js
@@ -5,15 +5,22 @@
         return { 
             templateUrl : 'backend/views/widget/list.html',
             link : function(scope,element,attrs) { 
-                scope.dbresult = ListWidget.getDbResults(); 
+                scope.dbresult = ListWidget.getDbResults();  
             }
 
         }
     }
 
+    function columnrender(ListWidget) {
+        return function(k) {
+            return ListWidget.getColumn(k).renderRow();
+        }
+    }
+
     angular
         .module('mean.backend')
-        .directive('listwidget', listwidget);
+        .directive('listwidget', listwidget)
+        .filter('columnrender',['ListWidget',columnrender]);
 
     listwidget.$inject = ['ListWidget'];
  
