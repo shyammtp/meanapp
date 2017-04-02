9197d6d3d58e7cdfa970fb36cdf9a8cc08eb5fd9
shyammtp <shyam.mtp@gmail.com>
2017-04-03 00:42:07 +0530

    Shyam Commit 3 Apr

---
+++ packages/custom/backend/server/models/itemmenu.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+arrayutil = require('../helpers/util').array,
+Schema = mongoose.Schema;
+
+
+var MenuItemSchema = new Schema({
+ 	menu_name : { 
+					en: {type: String}
+				},
+	menuurl : {type: String, required: true,lowercase : true},
+	sorting : {type: Number,default: 1},
+	status : {type : Boolean,default : false},
+	created_on : { type: Date, default: Date.now },
+    updated_on: { type: Date, default: Date.now }
+},{collection: "itemmenu"});
+  
+ 
+
+mongoose.model('MenuItem',MenuItemSchema);
