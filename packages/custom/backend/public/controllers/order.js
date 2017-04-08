9258c1964e40f9b7d4c7b391d5d11a968187faf6
shyammtp <shyam.mtp@gmail.com>
2017-04-08 14:46:14 +0530

    Shyam Commit

--- packages/custom/backend/public/controllers/order.js
+++ packages/custom/backend/public/controllers/order.js
@@ -6,7 +6,9 @@
         $scope.menuassign = {};
         $scope.menus = []; 
         $scope.items = []; 
+        $scope.cart = {};
         var items = {};
+        $scope.showvariant = {};
         function loopitems(its) {
             angular.forEach(its, function(v,l){
                 angular.forEach(v.menus,function(a,r){ 
@@ -22,7 +24,10 @@
                 items[v._id] = v;
             });
         }
-
+        $scope.$watch('cart.options',function() {
+            console.log($scope.cart.options);
+        })
+        var variationamoutsubtotal = {};
         $scope.menuitems = {};
         ItemMenus.getAllMenus().then(function(res){
             $scope.menus = res.data.menus;
@@ -38,8 +43,48 @@
         }
 
         $scope.addToCart = function(item, menu) {
+             $scope.showvariant[item._id] = false;
+            if(item.variantsetid) {
+                $scope.showvariant[item._id] = true;
+            } 
+
+            if(ArrayUtil.get($scope.showvariant,item._id) === true) {
+                if(validateoptions(item)) {
+                    //Add here to cart sending api
+                }
+            }
+
             console.log(item);
             console.log(menu);
+        }
+
+        var validateoptions = function(item) {
+            return true;
+        }
+
+        $scope.getFinalPrice = function(item) {             
+            return ArrayUtil.get(variationamoutsubtotal,item._id,item.data.standard_price);
+        }
+
+        $scope.updatePrice = function(setid, value, item) {
+            var changedoptions = $scope.cart.options[item._id];
+            //console.log(item);
+            console.log(item);
+            var optionset = item.variantsetid.option_set;
+            var sets = {};
+            angular.forEach(optionset,function(val,key) {
+                sets[val.id] = val.typedata.listvalues;
+            });
+            var variationamount = ArrayUtil.get(item.data,'standard_price',0);
+            angular.forEach(changedoptions,function(v,k) {
+                if(k in sets) { 
+                    var finald = sets[k][v];
+                    variationamount += ArrayUtil.get(finald,'price',0); 
+                }
+            });
+            variationamoutsubtotal[item._id] = variationamount;
+            console.log(variationamount);
+            console.log($scope.cart.options[item._id]);
         }
 
     }
