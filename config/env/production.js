ec3085398d669794d1b63b46f44be4b2c74bf05c
shyammtp <shyam.mtp@gmail.com>
2017-04-18 13:44:55 +0530

    Shyam Commit 17 Apr

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  db: process.env.MONGODB_URI,
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/shyamdb',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
@@ -77,5 +77,5 @@ module.exports = {
       pass: 'PASSWORD'
     }
   },
-  secret: 'SOME_TOKEN_SECRET'
+  secret: 'SHYAMSECRET'
 }
