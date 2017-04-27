7954a68c5a3a4ea6ada31260d3e20535efe7c429
shyammtp <shyam.mtp@gmail.com>
2017-04-28 02:21:19 +0530

    Shyam Commit 16 Apr 2017

--- packages/custom/backend/server/routes/backend.js
+++ packages/custom/backend/server/routes/backend.js
@@ -137,7 +137,7 @@ var Mongoose = require('mongoose'),
             
         app.post('/api/settings/save',authentic,settings.savesettings);
         app.post('/api/settings/saveall',authentic, settings.saveAllsettings);
-        app.get('/api/settings/get',authentic,settings.getallsettings);
+        app.get('/api/settings/get',settings.getallsettings);
         app.get('/api/settings/getpaginate',settings.getpaginate);
         app.get('/api/directory/get/country',settings.getcountry);
         app.get('/api/settings/getcountries',settings.getAllCountries);
